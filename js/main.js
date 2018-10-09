739d9548e38f0d1cf1ddf084ee547f397210037c
ejwillard <ejwillard@squiz.net>
2018-10-09 11:00:55 +1100

    fixed async issue with init func being called before being defined with a setTimeout and window.onload, not sure if this is the best solution though

--- js/main.js
+++ js/main.js
@@ -1,60 +1,67 @@
 console.log('hello again');
 
-// let googleMap;
+  // let googleMap;
 let myPlaces = [];
 
 
-//////////// init() function initializes the map element using Google Maps API, sets up the map click action and then tries to load markers from the localStorage.
+  //////////// init() function initializes the map element using Google Maps API, sets up the map click action and then tries to load markers from the localStorage.
 
 let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 36.2048, lng: 138.2529},
-    zoom: 6.8
-  });
 
-
-
-
-  map.markerList = [];
-  map.addListener('click', addPlace);
-
-  const placesFromLocalStorage = JSON.parse(localStorage.getItem('myPlaces'));
-  if (Array.isArray(placesFromLocalStorage)) {
-    myPlaces = placesFromLocalStorage; //if something was found
-    renderMarkers();                   //in the local storage,
-  }
-                                //set it as current place list.
-
-
-  //////////// addPlace() handles the map click — then adds new place to the list and invokes marker rendering.
-
-  function addPlace(event) {
-    myPlaces.push({
-      position: event.latLng
+  initMap = function() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 36.2048, lng: 138.2529},
+      zoom: 6.8
     });
 
-    localStorage.setItem('myPlaces', JSON.stringify(myPlaces)); //after marker is added, render it
-    renderMarkers();                                            // and synchronize local storage
-  }
 
 
-  ////////// renderMarkers() iterates through the places in the array and after clearing the map, puts the markers on it.
-
-  function renderMarkers() {
-    map.markerList.forEach(m => m.setMap(null)); //remove all markers
     map.markerList = [];
+    map.addListener('click', addPlace);
 
-    myPlaces.forEach((place) => {                     //add new markers
-      const marker = new google.maps.Marker({         //basing on "myPlaces"
-        position: place.position,                     //array elements
-        map: map
+    const placesFromLocalStorage = JSON.parse(localStorage.getItem('myPlaces'));
+    if (Array.isArray(placesFromLocalStorage)) {
+      myPlaces = placesFromLocalStorage; //if something was found
+      renderMarkers();                   //in the local storage,
+    }
+                                  //set it as current place list.
+
+
+    //////////// addPlace() handles the map click — then adds new place to the list and invokes marker rendering.
+
+    function addPlace(event) {
+      // document.getElementById('favPlacesList').textContent('<p>' + 'hey ' + place.name + 'Place ID: ' + place.place_id + '<br>' + place.formatted_address + '</p>')
+      // console.log('hellooooo')
+
+      myPlaces.push({
+        position: event.latLng
+
       });
 
-      map.markerList.push(marker);
-    });
-  }
-}
+      localStorage.setItem('myPlaces', JSON.stringify(myPlaces)); //after marker is added, render it
+      renderMarkers();                                            // and synchronize local storage
+    }
 
-initMap();
+
+    ////////// renderMarkers() iterates through the places in the array and after clearing the map, puts the markers on it.
+
+    function renderMarkers() {
+      map.markerList.forEach(m => m.setMap(null)); //remove all markers
+      map.markerList = [];
+
+      myPlaces.forEach((place) => {                     //add new markers
+        const marker = new google.maps.Marker({         //basing on "myPlaces"
+          position: place.position,                     //array elements
+          map: map
+        });
+
+        map.markerList.push(marker);
+      });
+    }
+  }
+
+window.onload = function() {
+  setTimeout(initMap,1000)
+    // initMap();
+  }
